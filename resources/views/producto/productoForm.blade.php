1cb55245d0075a06fca52a4f2dfeec45bd16dc13
Gabriel Lazo <gabriel.lazo.nc@gmail.com>
2023-03-29 00:18:41 -0600

    Se acomodaron los apartados de agregar y modificar productos

--- resources/views/producto/productoForm.blade.php
+++ resources/views/producto/productoForm.blade.php
@@ -1,28 +1,73 @@
-@extends('layouts.temp')
+@extends('layouts.windmill')
 @section('contenido')
-    <h1>Formulario de productos</h1>
+    <h4 class="mb-4 text-lg font-semibold text-gray-600 dark:text-gray-300">
+        Formulario de productos
+    </h4>
+    <div class="px-4 py-3 mb-8 bg-white rounded-lg shadow-md dark:bg-gray-800">
+        @if(isset($producto))
+            <!-- Edicion de programa -->
+            <form action="{{ route('producto.update', $producto)}}" method ="POST">
+                @method('PATCH')
+        @else
+            <!-- Creacion de programa -->
+            <form action="{{ route('producto.store')}}" method ="POST">
+        @endif    
 
-    <p>
-        <a href="{{ route('producto.index') }}">Listado de Productos</a>
-    </p>
-    @if(isset($producto))
-        <!-- Edicion de programa -->
-        <form action="{{ route('producto.update', $producto)}}" method ="POST">
-            @method('PATCH')
-    @else
-        <!-- Creacion de programa -->
-        <form action="{{ route('producto.store')}}" method ="POST">
-    @endif
-            @csrf
-            <label for="marca">Marca</label>
-            <input type="text" name="marca" id="marca" value="{{ $producto->marca ?? ''}}">
-            <br>
-            <label for="categoria">Categoria</label>
-            <input type="text" name="categoria" id="categoria" value="{{ $producto->categoria ?? ''}}">
-            <br>
-            <label for="folio">Folio</label>
-            <input type="text" name="folio" id="folio" value="{{ $producto->folio ?? ''}}">
-            <br>
-            <input type="submit" value="Guardar" value="{{ $producto->Guardar ?? ''}}">
+        @csrf
+        <label class="block text-sm">
+            <span class="text-gray-700 dark:text-gray-400">Marca del producto:</span>
+                <input
+                    class="block w-full mt-1 text-sm dark:border-gray-600 dark:bg-gray-700 focus:border-purple-400 focus:outline-none focus:shadow-outline-purple dark:text-gray-300 dark:focus:shadow-outline-gray form-input"
+                    type="text"
+                    name="marca"
+                    id="marca"
+                    value="{{ $producto->marca ?? ''}}"
+                />
+        </label>
+
+        <label class="block text-sm">
+            <span class="text-gray-700 dark:text-gray-400">Categoria del producto:</span>
+                <input
+                    class="block w-full mt-1 text-sm dark:border-gray-600 dark:bg-gray-700 focus:border-purple-400 focus:outline-none focus:shadow-outline-purple dark:text-gray-300 dark:focus:shadow-outline-gray form-input"
+                    type="text"
+                    name="categoria"
+                    id="categoria"
+                    value="{{ $producto->marca ?? ''}}"
+                />
+        </label>
+
+        <label class="block text-sm">
+            <span class="text-gray-700 dark:text-gray-400">Folio del producto:</span>
+                <input
+                    class="block w-full mt-1 text-sm dark:border-gray-600 dark:bg-gray-700 focus:border-purple-400 focus:outline-none focus:shadow-outline-purple dark:text-gray-300 dark:focus:shadow-outline-gray form-input"
+                    type="text"
+                    name="folio"
+                    id="folio"
+                    value="{{ $producto->categoria ?? ''}}"
+                />
+        </label>
+
+        <div class="mt-4">
+                <button class="flex items-center justify-between px-4 py-2 text-sm font-medium leading-5 text-white transition-colors duration-150 bg-blue-600 border border-transparent rounded-lg active:bg-blue-600 hover:bg-blue-700 focus:outline-none focus:shadow-outline-blue">
+                <svg fill="none" stroke="currentColor" stroke-width="1.5" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
+  <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 14.25v-2.625a3.375 3.375 0 00-3.375-3.375h-1.5A1.125 1.125 0 0113.5 7.125v-1.5a3.375 3.375 0 00-3.375-3.375H8.25m.75 12l3 3m0 0l3-3m-3 3v-6m-1.5-9H5.625c-.621 0-1.125.504-1.125 1.125v17.25c0 .621.504 1.125 1.125 1.125h12.75c.621 0 1.125-.504 1.125-1.125V11.25a9 9 0 00-9-9z"></path>
+</svg>
+                
+                <!-- <svg class="w-4 h-4 mr-2 -ml-1" fill="currentColor" aria-hidden="true" viewBox="0 0 20 20"> -->
+                    <!-- <path d="M3.172 5.172a4 4 0 015.656 0L10 6.343l1.172-1.171a4 4 0 115.656 5.656L10 17.657l-6.828-6.829a4 4 0 010-5.656z" clip-rule="evenodd" fill-rule="evenodd"></path> -->
+                  <!-- </svg> -->
+                  <span>Icon left</span>
+                </button>
+              </div>
+
+        <div class="mt-4">
+            <input 
+                class="px-4 py-2 text-sm font-medium leading-5 text-white transition-colors duration-150 bg-blue-600 border border-transparent rounded-lg active:bg-blue-600 hover:bg-blue-700 focus:outline-none focus:shadow-outline-blue"
+                type="submit" 
+                value="Guardar">
+            </input>
+        </div>
+
         </form>
+    </div>
 @endsection
--- resources/views/producto/productoIndex.blade.php
+++ resources/views/producto/productoIndex.blade.php
@@ -1,34 +1,94 @@
-@extends('layouts.temp')
+@extends('layouts.windmill')
 @section('contenido')
-    <h1>Listado de productos</h1>
-    <p>
-        <a href="{{ route('producto.create') }}">Agregar producto</a>
-    </p>
+    <h2 class="my-6 text-2xl font-semibold text-gray-700 dark:text-gray-200">
+        Listado de productos
+    </h2>
 
-    <table border="2">
-        <thead>
-            <tr>
-                <th>ID</th>
-                <th>Marca</th>
-                <th>Categoria</th>
-                <th>Folio</th>
-                <th>Acciones</th>
+    <div>
+        
+    </div>
+
+    <!-- With actions -->
+    <h4 class="mb-4 text-lg font-semibold text-gray-600 dark:text-gray-300">
+        <a class="px-4 py-2 text-sm font-medium leading-5 text-white transition-colors duration-150 bg-blue-600 border border-transparent rounded-lg active:bg-blue-600 hover:bg-blue-700 focus:outline-none focus:shadow-outline-blue"
+            href="{{ route('producto.create') }}">
+                Agregar producto
+        </a>
+    </h4>
+    <div class="w-full overflow-hidden rounded-lg shadow-xs">
+        <div class="w-full overflow-x-auto">
+        <table class="w-full whitespace-no-wrap">
+            <thead>
+            <tr
+                class="text-xs font-semibold tracking-wide text-left text-gray-500 uppercase border-b dark:border-gray-700 bg-gray-50 dark:text-gray-400 dark:bg-gray-800"
+            >
+                <!-- <th class="px-4 py-3">ID</th> -->
+                <th class="px-4 py-3">Marca</th>
+                <th class="px-4 py-3">Categoria</th>
+                <th class="px-4 py-3">Folio</th>
+                <th class="px-4 py-3">Acciones</th>
             </tr>
-        </thead>
-        <tbody>
+            </thead>
+            <tbody
+            class="bg-white divide-y dark:divide-gray-700 dark:bg-gray-800"
+            >
             @foreach($productos as $producto)
-                <tr>
-                    <td>{{$producto->id}}</td>
-                    <td>
-                        <a href="{{ route('producto.show', $producto->id) }}">{{$producto->marca}}</a>
-                    </td>
-                    <td>{{$producto->categoria}}</td>
-                    <td>{{$producto->folio}}</td>
-                    <td>
-                        <a href="{{ route('producto.edit', $producto) }}">Editar</a>
-                    </td>
-                </tr>
-            @endforeach
+            <tr class="text-gray-700 dark:text-gray-400">
+                <td class="px-4 py-3">
+                <div class="flex items-center text-sm">
+                    <a href="{{ route('producto.show', $producto->id) }}">{{$producto->marca}}</a>
+                </div>
+                </td>
+                <td class="px-4 py-3 text-sm">
+                    {{$producto->marca}}
+                </td>
+                <td class="px-4 py-3 text-xs">
+                    {{$producto->categpria}}
+                </td>
+                <td class="px-4 py-3 text-sm">
+                    {{$producto->folio}}
+                </td>
+                <td class="px-4 py-3">
+                    <div class="flex items-center space-x-4 text-sm">
+                        <a
+                            class="flex items-center justify-between px-2 py-2 text-sm font-medium leading-5 text-purple-600 rounded-lg dark:text-gray-400 focus:outline-none focus:shadow-outline-gray"
+                            aria-label="Edit"
+                            href="{{ route('producto.edit', $producto) }}"
+                        >
+                        <svg
+                            class="w-5 h-5"
+                            aria-hidden="true"
+                            fill="currentColor"
+                            viewBox="0 0 20 20"
+                        >
+                            <path
+                            d="M13.586 3.586a2 2 0 112.828 2.828l-.793.793-2.828-2.828.793-.793zM11.379 5.793L3 14.172V17h2.828l8.38-8.379-2.83-2.828z"
+                            ></path>
+                        </svg>
+                        </a>
+                        <button
+                        class="flex items-center justify-between px-2 py-2 text-sm font-medium leading-5 text-purple-600 rounded-lg dark:text-gray-400 focus:outline-none focus:shadow-outline-gray"
+                        aria-label="Delete"
+                        >
+                        <svg
+                            class="w-5 h-5"
+                            aria-hidden="true"
+                            fill="currentColor"
+                            viewBox="0 0 20 20"
+                        >
+                            <path
+                            fill-rule="evenodd"
+                            d="M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2 2 0 002 2h8a2 2 0 002-2V6a1 1 0 100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v6a1 1 0 102 0V8a1 1 0 00-1-1z"
+                            clip-rule="evenodd"
+                            ></path>
+                        </svg>
+                        </button>
+                    </div>
+                </td>
+            </tr>
+@endforeach
         </tbody>
-    </table>
+    </table>  
+</div> 
+</div>
 @endsection
